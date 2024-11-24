52f0a66c6e12f200b187823f4b9d7ac2dac733ac
Ravi Kumar Mandala <ravikumar.mandala@gmail.com>
2024-11-23 21:16:38 -0800

    Renamed the variable i to j

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,8 @@ class HelloWorld {
     System.out.println("Hello World!!!");
     System.out.println("Welcome to the future!");
 
-    for (int i = 1; i <= 100; i++) {
-      System.out.println(i);
+    for (int j = 1; j <= 100; j++) {
+      System.out.println(j);
     }
 
     System.out.print("Collaborators: Ravi Mandala, Ravi Mandala test1");
