6ffd9dad7f527e9b6d586daad1a9b6b77c26c37e
Ravi Kumar Mandala <ravikumar.mandala@gmail.com>
2024-11-23 21:38:35 -0800

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,13 @@
+class HelloWorld:
+    @staticmethod
+    def main():
+        print("Hello World!!!")
+        print("Welcome to the future!")
+
+        for j in range(1, 101):
+            print(f"The number is {j}")
+
+        print("Collaborators: Ravi Mandala, Ravi Mandala test1")
+
+if __name__ == "__main__":
+    HelloWorld.main()
