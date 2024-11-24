0938ec5e80161b066d404623d994acb98fb8e3fc
Ravi Kumar Mandala <ravikumar.mandala@gmail.com>
2024-11-23 20:32:57 -0800

    Adding HelloWorld java file

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello World!!!");
+    System.out.println("Welcome to the future!");
+
+    for (int i = 1; i <= 100; i++) {
+      System.out.println(i);
+    }
+  }
+}
